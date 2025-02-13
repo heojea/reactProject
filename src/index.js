3c617e78a90fe4ae97deddeab20ffa1ac77ca053
p.jyheokalmate.net <p.jyheo@kalmate.net>
2025-02-13 15:44:35 +0900

    리엑트 프로젝트 최초 커밋

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  /* react strictMode 빌드과정에서 오류나 사용되지 않는 코드를 잡아내는 빌드 체크 활성화 라고 보면 됨 */
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
